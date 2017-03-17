8604acbeff35df079023bb0182fd419b410bbc35
Theofilos Axiotis <theofilos.ax@gmail.com>
2017-03-17 14:19:25 +0200

    Update db_connection.php

--- e-commerce/db_connection.php
+++ e-commerce/db_connection.php
@@ -1,5 +1,5 @@
 <?php
-$connection = mysqli_connect('localhost', '*', '*', '*');
+$connection = mysqli_connect('localhost', 'username', 'password', 'database');
 mysqli_set_charset($connection, 'utf8');
 if (!$connection) {
     die('Could not connect: ' . mysqli_error($connection));
